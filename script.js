eb9903ddb5db3319c6df39629c7261445544685b
thadeu-alves <thadeutelecentrosg@gmail.com>
2024-09-11 09:32:27 -0300

    style and del func add

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const list = document.querySelector('ul');
 const saveButton = document.getElementById('saveLink');
-
 update();
 
 saveButton.addEventListener('click', function() {
+
+
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const currentTab = tabs[0];
 
@@ -29,10 +30,42 @@ function update() {
 
     list.innerHTML = '';
 
-    links.forEach(link => {
+    links.forEach((link, index) => {
+      let profile = getDomainFromURL(link);
+      if(profile == undefined){return;}
       const li = document.createElement('li');
-      li.innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+      li.innerHTML = `<a href="${link}" target="_blank">${profile}</a> <button data-index="${index}">rem</button>`;
       list.appendChild(li);
+    });
+
+    const removeButtons = document.querySelectorAll('button[data-index]');
+    removeButtons.forEach(button => {
+      button.addEventListener('click', function() {
+        const index = this.getAttribute('data-index');
+        removeLink(index);
+      });
     });
   });
 }
+
+function removeLink(index) {
+  chrome.storage.local.get({ savedLinks: [] }, function(result) {
+    const links = result.savedLinks;
+    links.splice(index, 1); // Remove o link da lista
+
+    chrome.storage.local.set({ savedLinks: links }, function() {
+      if (chrome.runtime.lastError) {
+        console.error("Erro ao remover o link: ", chrome.runtime.lastError);
+      } else {
+        console.log('Link removido com sucesso');
+        update(); // Atualiza a lista após a remoção
+      }
+    });
+  });
+}
+
+function getDomainFromURL(url) {
+    const urlObject = new URL(url);
+    const pathSegments = urlObject.pathname.split('/');
+    return pathSegments[2];
+  }
